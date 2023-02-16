f6f94f6cadc669de7d106ccba57d3f083cb74b4f
Nicolas Stabilini <stabilini@gmail.com>
2023-02-16 13:37:02 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import { HangmanDrawing } from "./components/HangmanDrawing"
+import { HangmanWord } from "./components/HangmanWord"
+import { Keyboard } from "./components/Keyboard"
+import words from './wordList.json'
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  return (
+    <div style={{ maxWidth: '800px', display: 'flex', flexDirection: 'column', gap: '2rem', margin: '0 auto', alignItems: 'center'}}>
+      <div style={{ fontSize: '2rem', textAlign: 'center' }}>
+        Lose
+        Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HangmanWord.tsx
@@ -0,0 +1,5 @@
+export function HangmanWord() {
+  return (
+    <></>
+  )
+}
